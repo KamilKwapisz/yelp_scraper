094dd88b6e5012aaf886325cd61fb03ab1c28f13
KamilKwapisz <kamiltm1998@gmail.com>
2020-03-16 15:33:41 +0100

    basic spider is now download basic data

--- yelp/settings.py
+++ yelp/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'yelp.spiders'
 #USER_AGENT = 'yelp (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 8
@@ -39,10 +39,11 @@ CONCURRENT_REQUESTS = 8
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Language': 'en',
+  'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
+}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
--- yelp/spiders/us_spider.py
+++ yelp/spiders/us_spider.py
@@ -9,4 +9,23 @@ class SpiderUS(scrapy.Spider):
     ]
 
     def parse(self, response):
-        pass
+        name = response.css("h1::text").get()
+
+        categories = response.xpath(
+            """//*[@id="wrap"]/div[3]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[1]/div/div/span[2]/span/a/text()"""
+        ).extract()
+        category = " > ".join(categories)
+
+        phone = response.xpath(
+            """//*[@id="wrap"]/div[3]/div/div[1]/div[3]/div/div/div[2]/div[2]/div/div/section[2]/div/div[2]/div/div[2]/p[2]/text()"""
+        ).get()
+
+        yield {
+            'name': name,
+            'category': category,
+            'phone': phone,
+        }
+
+
+
+
